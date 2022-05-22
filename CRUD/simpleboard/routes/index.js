8c5d9dda22e7dc6291e962d346d48f45febde883
about2weeks <its.about2weeks@gmail.com>
2022-05-22 21:39:59 +0900

    add login auth with google

--- CRUD/simpleboard/routes/index.js
+++ CRUD/simpleboard/routes/index.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const asyncHandler = require('../utils/async-handler');
 const hashPassword = require('../utils/hash-password');
+const sendMail = require('../utils/send-mail');
+const generateRandomPassword = require('../utils/generate-random-password')
 const { User } = require('../models');
 
 const router = Router();
@@ -35,11 +37,56 @@ router.post('/join', asyncHandler(async (req, res) => {
 }));
 
 router.get('/logout', (req, res, next) => {
-  req.logout();
-  req.session.save((err) => {
-    if (err) throw err;
-    res.redirect('/');
-  });
+  res.cookie('token', null, { maxAge: 0 });
+  res.redirect('/');
 });
+
+router.get('/reset-password', (req, res, next) => {
+  res.render('user/reset-password');
+});
+
+router.post('/reset-password', asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('해당 메일로 가입된 사용자가 없습니다.');
+  }
+  
+  // 랜덤 패스워드 생성하기
+  const password = generateRandomPassword();
+  
+  
+  await User.updateOne({ email }, {
+    // hashPassword 로 업데이트 하기
+    password: hashPassword(password),
+    // passwordReset
+    passwordReset: true,
+  });
+  
+  await sendMail(email, "비밀번호가 변경되었습니다.", `변경된 비밀번호는: ${password}입니다.`);
+  res.send("메일이 발송되었습니다.");
+  
+  res.render('user/reset-password-confirmed');
+}));
+
+router.get('/change-password', (req, res, next) => {
+  res.render('user/change-password');
+});
+
+router.post('/change-password', asyncHandler(async (req, res) => {
+  const { currentPassword, password } = req.body;
+  const user = await User.findOne({ shortId: req.user.shortId });
+  
+  if (user.password !== hashPassword(currentPassword)) {
+    throw new Error('임시 비밀번호가 일치하지 않습니다.');
+  }
+  
+  await User.updateOne({ shortId: user.shortId }, {
+    password: hashPassword(password),
+    passwordReset: false,
+  });
+  
+  res.redirect('/logout');
+}));
 
 module.exports = router;
